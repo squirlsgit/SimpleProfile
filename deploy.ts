12e88855198f48f56a2dd25a49f48d4799f1394d
Nich-at-790999098857 <nich.t.j@gmail.com>
2019-05-24 00:53:20 -0400

    initial commit deploy

---
+++ deploy.ts
@@ -0,0 +1,32 @@
+import { spawn } from 'child_process';
+declare var process;
+const now = new Date();
+
+let branch = process.argv0 || 'master';
+//if (process.argv[2]) {
+//  branch = process.argv[2];
+//}
+console.log("Using Branch", branch);
+const child = spawn(`
+  npm run-script build &&
+  git add . &&
+  git commit -am "Updating code at ${now.toUTCString()} and publishing" &&
+  git push heroku ${branch}
+`);
+
+child.stdout.setEncoding('utf8');
+let log = 0;
+
+child.stdout.on('data', (data) => {
+  log++;
+  console.log(`${log}:`, data);
+});
+
+child.stderr.on('error', (err) => {
+  console.error(`${log}`, err);
+  child.kill();
+});
+
+child.on('close', (closing) => {
+  console.log(`Child process exited with code ${closing}`);
+});
---
+++ server.ts
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+
+
+const app = express();
+
+app.use(express.static(__dirname + '/production'));
+
+app.get('/*', function(req,res){
+
+  res.sendFile(path.join(__dirname, 'production', 'index.html'));
+
+});
+
+// Start the app by listening on the default Heroku port
+
+app.listen(process.env.PORT || 8080);
+
