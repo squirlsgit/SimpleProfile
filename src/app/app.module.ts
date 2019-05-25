194200ed2eaf24ed0a25a54805db8ce27fa96a24
squirlsgit <nich.t.j@gmail.com>
2019-05-25 18:56:58 -0400

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { CommonModule } from "@angular/common";
+import { AppComponent } from './main/app.component';
 import AngularComponents from '../components/angularComponents';
 @NgModule({
   declarations: [
@@ -10,8 +9,8 @@ import AngularComponents from '../components/angularComponents';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     AngularComponents,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/components/angularComponents.ts
+++ src/components/angularComponents.ts
@@ -1,8 +1,9 @@
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon'; 
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule],
-  exports: [MatButtonModule, MatCheckboxModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatIconModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatIconModule],
 })
 export default class AngularComponents { }
